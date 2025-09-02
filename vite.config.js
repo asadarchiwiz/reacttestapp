61e99fe5199e4ae126cfb0b9480890f4b490d575
asadarchiwiz <asad@archiwiz.com>
2025-09-02 13:15:21 +0500

    3rd commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8080,   // 👈 runs dev server on http://localhost:8080
+  },
 })
