20f56dcb79fcd2282514f5cbf18c2d96100c27ba
asadarchiwiz <asad@archiwiz.com>
2025-09-04 13:52:55 +0500

    feate : testing app

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite Asad khan</h1>
+      <h1>Vite Asad khan</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
